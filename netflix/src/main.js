b4c74111ef915823695296f79b97d1ebefbf3699
vladkasyan <125082821+vladkasyan@users.noreply.github.com>
2025-06-10 16:22:49 +0300

    update

--- netflix/src/main.js
+++ netflix/src/main.js
@@ -1,1 +1,8 @@
 
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+burger.addEventListener('click', () => {
+  nav.classList.toggle('nav-active');
+  burger.classList.toggle('active');
+}
+);
